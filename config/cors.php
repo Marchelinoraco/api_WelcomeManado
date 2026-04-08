8264f101deff2dabeda3992973b5c5897ae57479
lino <linoraco164@gmail.com>
2026-04-08 15:50:01 +0800

    cors domain

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,8 @@ return [
         'http://127.0.0.1:5174',
         'https://client-welcome-manado.vercel.app',
         'https://admin-welcome-manado.vercel.app',
+        'https://welcomemanado.my.id',
+        'https://admin.welcomemanado.my.id',
     ]))))),
     'allowed_origins_patterns' => array_filter(array_map('trim', explode(',', (string) env('CORS_ALLOWED_ORIGINS_PATTERNS', implode(',', [
         '/^http:\/\/localhost:\d+$/',
